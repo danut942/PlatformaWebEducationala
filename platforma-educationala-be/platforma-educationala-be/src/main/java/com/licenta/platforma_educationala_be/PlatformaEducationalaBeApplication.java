b5fd2edf32b66f3363d333a074214e4aa131c783
Danut Sacu <danut.sacu@gmail.com>
2024-08-20 03:21:41 +0300

    adauga fisiere licenta

---
+++ platforma-educationala-be/platforma-educationala-be/src/main/java/com/licenta/platforma_educationala_be/PlatformaEducationalaBeApplication.java
@@ -0,0 +1,13 @@
+package com.licenta.platforma_educationala_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformaEducationalaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformaEducationalaBeApplication.class, args);
+	}
+
+}
---
+++ platforma-educationala-be/platforma-educationala-be/src/test/java/com/licenta/platforma_educationala_be/PlatformaEducationalaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenta.platforma_educationala_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformaEducationalaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
